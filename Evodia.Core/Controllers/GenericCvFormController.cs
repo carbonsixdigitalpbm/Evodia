9c5a08c3f1f7ccde6c27c9e2a6d00d24872212e8
Paulius Putna <up614961@myport.ac.uk>
2016-11-22 20:43:53 +0000

    GenericCvForm

---
+++ Evodia.Core/Controllers/GenericCvFormController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Web.Mvc;
+using Umbraco.Core.Logging;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+using Umbraco.Core;
+using Evodia.Core.Models;
+using Evodia.Core.Utility;
+using Umbraco.Core.Services;
+
+namespace Evodia.Core.Controllers
+{
+    public class GenericCvFormController : SurfaceController
+    {
+        private readonly MailHelper _mailHelper = new MailHelper();
+        private readonly IContentService _contentService = ApplicationContext.Current.Services.ContentService;
+        private readonly UmbracoHelper _umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
+        private const int FormFolderId = 1155;
+
+        public ActionResult RenderGenericCvForm()
+        {
+            return PartialView("~/Views/Partials/Forms/GenericCvForm.cshtml", new GenericCvForm());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ProcessFormSubmission(GenericCvForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["ValidationFailed"] = "The form validation could not pass. Please check your input.";
+
+                return CurrentUmbracoPage();
+            }
+
+            TempData["ValidationPasses"] = "The form has been validated successfully.";
+            TempData["FormFolderId"] = FormFolderId;
+
+            //SaveContactFormSubmission(model);
+            //SendEmailNotifications(model);
+
+            if (_umbracoHelper.TypedContent(FormFolderId).HasValue("redirectPage"))
+            {
+                return RedirectToUmbracoPage(_umbracoHelper.TypedContent(FormFolderId).GetPropertyValue<int>("redirectPage"));
+            }
+
+            return RedirectToCurrentUmbracoPage();
+        }
+
+        //private void SaveContactFormSubmission(GenericCvForm model)
+        //{
+        //    try
+        //    {
+        //        var formSubmission = _contentService.CreateContent(model.Name + ", " + model.Email + " - " + DateTime.Now.ToShortDateString(), FormFolderId, "contactForm");
+
+        //        formSubmission.SetValue("name", model.Name);
+        //        formSubmission.SetValue("emailAddress", model.Email);
+        //        formSubmission.SetValue("message", model.Message);
+        //        _contentService.SaveAndPublishWithStatus(formSubmission);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        LogHelper.Warn(GetType(), "Contact form saving failed with the exception: " + ex.Message);
+        //    }
+
+        //}
+
+        private void SendEmailNotifications(ContactForm model)
+        {
+            var formFolder = _umbracoHelper.TypedContent(FormFolderId);
+
+            if (!string.IsNullOrEmpty(formFolder.Name))
+            {
+                _mailHelper.CreateAndSendNotifications(model, formFolder);
+            }
+            else
+            {
+                LogHelper.Warn(GetType(), "Couldn't get the form folder with the id: " + FormFolderId);
+            }
+        }
+    }
+}
---
+++ Evodia.Core/Models/GenericCvForm.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Evodia.Core.Models
+{
+    public class GenericCvForm
+    {
+        [DisplayName("First name")]
+        [Required(ErrorMessage = "Please enter your first name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Second name")]
+        [Required(ErrorMessage = "Please enter your second name")]
+        public string SecondName { get; set; }
+
+        [DisplayName("Email address")]
+        [Required(ErrorMessage = "Please enter your email address")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [DisplayName("Security clearance level")]
+        public string SecurityClearanceLevel { get; set; }
+
+        [DisplayName("Job preference")]
+        public List<SelectListItem> JobPreference { get; set; }
+
+        public string SelectedJobPreference { get; set; }
+
+        [DisplayName("Availability")]
+        public string Availability { get; set; }
+
+        //[Display(Name = "Your CV (Pdf or word file)")]
+        //[Required(ErrorMessage = "Please attach your CV")]
+        //public HttpPostedFileBase Attachment { get; set; }
+    }
+}
