562091f2464a7879efc023bd5586fd0d0961dfee
kevsimpson <hello@kev-simpson.me>
2018-01-03 14:43:34 +0000

    Update header styles and add card links to home

---
+++ Evodia.Core/Models/CardViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Umbraco.Core.Models;
+
+namespace Evodia.Core.Models
+{
+    public class CardViewModel
+    {
+        public string Headline { get; set; }
+
+        public IPublishedContent Image { get; set; }
+
+        public string Description { get; set; }
+        
+        public string Url { get; set; }
+
+        public string Target { get; set; }
+
+        public string CtaName { get; set; }
+
+        public string ModifierClass { get; set; }
+    
+        public CardViewModel()
+        {
+            CtaName = "Read more";
+        }
+    }
+
+}
