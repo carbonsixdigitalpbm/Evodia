7f9c5fde3db0459a885fcc1048b291d04eb1fcc9
kevsimpson <hello@kev-simpson.me>
2018-01-04 10:31:21 +0000

    Add masthead component

---
+++ Evodia.Core/Models/HeadingViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Evodia.Core.Models
+{
+    public class HeadingViewModel
+    {
+        public string Headline { get; set; }
+
+        public string SubHeadline { get; set; }
+
+        public string Level { get; set; }
+
+        public string ModifierClass { get; set; }
+
+        public HeadingViewModel()
+        {
+            Level = "h2";
+        }
+    }
+
+}
